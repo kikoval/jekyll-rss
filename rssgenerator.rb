8ac5a30d32ad34a07318366ee5510ff281f5dab1
Assaf Gelber <assaf.gelber@gmail.com>
2013-07-25 20:21:12 +0300

    Fixed indentation

--- rssgenerator.rb
+++ rssgenerator.rb
@@ -20,75 +20,75 @@
 # Copyright Assaf Gelber 2013
 
 module Jekyll
-	class RssFeed < Page;	end
+  class RssFeed < Page; end
 
-	class RssGenerator < Generator
-		priority :low
-		safe true
+  class RssGenerator < Generator
+    priority :low
+    safe true
 
-		# Generates an rss 2.0 feed
-		#
-		# site - the site
-		#
-		# Returns nothing
-		def generate(site)
-			require 'rss'
+    # Generates an rss 2.0 feed
+    #
+    # site - the site
+    #
+    # Returns nothing
+    def generate(site)
+      require 'rss'
 
-			# Create the rss with the help of the RSS module
-			rss = RSS::Maker.make("2.0") do |maker|
-				maker.channel.title = site.config['name']
-				maker.channel.link = site.config['url']
-				maker.channel.description = site.config['description'] || "RSS feed for #{site.config['name']}"
-				maker.channel.author = site.config["author"]
-				maker.channel.updated = site.posts.map { |p| p.date  }.max
-				maker.channel.copyright = site.config['copyright']
-				site.posts.each do |post|
-					maker.items.new_item do |item|
-						item.title = post.title
-						item.link = "#{site.config['url']}#{post.url}"
-						item.description = post.excerpt
-						item.updated = post.date
-					end
-				end
-			end
+      # Create the rss with the help of the RSS module
+      rss = RSS::Maker.make("2.0") do |maker|
+        maker.channel.title = site.config['name']
+        maker.channel.link = site.config['url']
+        maker.channel.description = site.config['description'] || "RSS feed for #{site.config['name']}"
+        maker.channel.author = site.config["author"]
+        maker.channel.updated = site.posts.map { |p| p.date  }.max
+        maker.channel.copyright = site.config['copyright']
+        site.posts.each do |post|
+          maker.items.new_item do |item|
+            item.title = post.title
+            item.link = "#{site.config['url']}#{post.url}"
+            item.description = post.excerpt
+            item.updated = post.date
+          end
+        end
+      end
 
-			# File creation and writing
-			rss_path = ensure_slashes(site.config['rss_path'] || "/")
-			rss_name = site.config['rss_name'] || "rss.xml"
-			full_path = File.join(site.dest, rss_path)
-			ensure_dir(full_path)
-			File.open("#{full_path}#{rss_name}", "w") { |f| f.write(rss) }
+      # File creation and writing
+      rss_path = ensure_slashes(site.config['rss_path'] || "/")
+      rss_name = site.config['rss_name'] || "rss.xml"
+      full_path = File.join(site.dest, rss_path)
+      ensure_dir(full_path)
+      File.open("#{full_path}#{rss_name}", "w") { |f| f.write(rss) }
 
-			# Add the feed page to the site pages
-			site.pages << Jekyll::RssFeed.new(site, site.dest, rss_path, rss_name)
-		end
+      # Add the feed page to the site pages
+      site.pages << Jekyll::RssFeed.new(site, site.dest, rss_path, rss_name)
+    end
 
-		private
-		# Ensures the given path has leading and trailing slashes
-		#
-		# path - the string path
-		#
-		# Return the path with leading and trailing slashes
-		def ensure_slashes(path)
-			ensure_leading_slash(ensure_trailing_slash(path))
-		end
+    private
+    # Ensures the given path has leading and trailing slashes
+    #
+    # path - the string path
+    #
+    # Return the path with leading and trailing slashes
+    def ensure_slashes(path)
+      ensure_leading_slash(ensure_trailing_slash(path))
+    end
 
-		# Ensures the given path has a leading slash
-		#
-		# path - the string path
-		#
-		# Returns the path with a leading slash
-		def ensure_leading_slash(path)
-			path[0] == "/" ? path : "/#{path}"
-		end
+    # Ensures the given path has a leading slash
+    #
+    # path - the string path
+    #
+    # Returns the path with a leading slash
+    def ensure_leading_slash(path)
+      path[0] == "/" ? path : "/#{path}"
+    end
 
-		# Ensures the given path has a trailing slash
-		#
-		# path - the string path
-		#
-		# Returns the path with a trailing slash
+    # Ensures the given path has a trailing slash
+    #
+    # path - the string path
+    #
+    # Returns the path with a trailing slash
     def ensure_trailing_slash(path)
-    	path[-1] == "/" ? path : "#{path}/"
+      path[-1] == "/" ? path : "#{path}/"
     end
 
     # Ensures the given directory exists
@@ -97,8 +97,8 @@ module Jekyll
     #
     # Returns nothing
     def ensure_dir(path)
-    	FileUtils.mkdir_p(path)
+      FileUtils.mkdir_p(path)
     end
 
-	end
+  end
 end
